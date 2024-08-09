4c096fabfe4ac14f59a1b977b631494eab9aa8a2
Satyadev Maurya <in.satyadev@gmail.com>
2024-08-09 23:39:21 +0530

    add button for product quantity

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,6 @@ import React from "react";
 import Product from "./Product.js";
 
 export default function ProductList(props){
-    console.log(props)
   return (
     props.productList.map((product, i) => {
     return <Product product={product} key={i}/>
