ea2a430ed0be139aceb54420b7fc60555cab2d87
Satyadev Maurya <in.satyadev@gmail.com>
2024-09-06 21:52:37 +0530

    addItem constructor

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,21 +1,60 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+
 class addItem extends Component {
-    state = {  } 
-    render() { 
-        return <form className="row mb-5">
+  constructor(props) {
+    super(props);
+    this.state = {
+      productName: "",
+      productPrice: 0,
+    };
+  }
+  render() {
+    return (
+      <form className="row mb-5" onSubmit={(e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.productName,Number(this.state.productPrice));
+      }}>
         <div className="mb-3 col-4">
-          <label htmlFor="inputName" className="form-label">Email address</label>
-          <input type="text" className="form-control" id="inputName" aria-describedby="name"/>
+          <label htmlFor="inputName" className="form-label">
+            Email address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            aria-describedby="name"
+            name="productName"
+            onChange={(e) => {
+              this.setState({ productName: e.currentTarget.value });
+            }}
+            value={this.state.productName}
+
+          />
         </div>
         <div className="mb-3 col-4">
-          <label htmlFor="inputPrice" className="form-label">Price</label>
-          <input type="number" className="form-control" id="price"/>
+          <label htmlFor="inputPrice" className="form-label">
+            Price
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="price"
+            name="productPrice"
+            onChange={(e) => {
+              this.setState({ productPrice: Number(e.currentTarget.value) });
+            }}
+            value={this.state.productPrice}
+          />
         </div>
-        <button type="submit" className="btn btn-primary col-4" onClick={() => {
-          this.props.addItem()
-        }}>Add</button>
-      </form>;
-    }
+        <button
+          type="submit"
+          className="btn btn-primary col-4"
+        >
+          Add
+        </button>
+      </form>
+    );
+  }
 }
- 
+
 export default addItem;
